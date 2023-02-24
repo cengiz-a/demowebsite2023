884cf45cb1f3bfea955b8cc93e717d7187bad216
cengiz-a <a.c.cengizhan@gmail.com>
2023-02-24 06:03:20 +0100

    all

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import '../scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+import heroBanner from '../../public/AdobeStock_336795357.png'
+import logo from '../../public/logo.png'
+
+
+
+
+// assign graphics
+
+document.getElementById('hero-bg-img').src = heroBanner;
+document.getElementById('logo-top').src = logo;
+document.getElementById('logo-buttom').src = logo;
